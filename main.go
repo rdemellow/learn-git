5ad01dd7cd358d3590809ec4650328b5c68d8c3c
Richard de Mellow <richard.demellow@couchbase.com>
2022-10-26 08:30:59 +0100

    Print Foo on factors of 3

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ func main() {
     maxNum := 100
 
     for i := 0; i < maxNum; i++ {
-        fmt.Println(i);
+        if i % 3 == 0 {
+            fmt.Println("Foo");
+        } else {
+            fmt.Println(i);
+        }
     }
 }
